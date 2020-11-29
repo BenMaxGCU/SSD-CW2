5477002a6730fbaed52c0afc4b6839118c613838
BenMaxGCU <bmaxwe200@caledonian.ac.uk>
2020-11-29 21:14:31 +0000

    Fixed Login Issue

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace cw2_ssd.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("TicketConnection", throwIfV1Schema: false)
         {
         }
 
